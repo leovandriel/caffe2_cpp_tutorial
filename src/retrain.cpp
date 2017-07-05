e178c50684943d900a280b3be877edd6e542b7cb
Leo Vandriel <git@leovandriel.com>
2017-07-05 11:34:56 -0700

    add net split to retain

--- src/retrain.cpp
+++ src/retrain.cpp
@@ -1,3 +1,5 @@
+#include "util/net.h"
+
 #include "caffe2/core/init.h"
 #include "caffe2/utils/proto_utils.h"
 #include "caffe2/core/db.h"
@@ -11,11 +13,8 @@
 #include "operator/operator_cout.h"
 #include "res/imagenet_classes.h"
 
-#include <dirent.h>
-#include <sys/stat.h>
-
 CAFFE2_DEFINE_string(model, "", "Name of one of the pre-trained models.");
-CAFFE2_DEFINE_string(blob, "", "Name of the blob on which to split the model.");
+CAFFE2_DEFINE_string(layer, "", "Name of the layer on which to split the model.");
 CAFFE2_DEFINE_string(folder, "", "Folder with subfolders with images");
 
 CAFFE2_DEFINE_string(db_type, "leveldb", "The database type.");
@@ -27,200 +26,7 @@ CAFFE2_DEFINE_double(learning_rate, 1e-4, "Learning rate.");
 CAFFE2_DEFINE_string(optimizer, "adam", "Training optimizer: sgd/momentum/adagrad/adam");
 CAFFE2_DEFINE_bool(force_cpu, false, "Only use CPU, no CUDA.");
 
-enum {
-  kRunTrain = 0,
-  kRunValidate = 1,
-  kRunTest = 2,
-  kRunNum = 3
-};
-
-static std::map<int, std::string> name_for_run({
-  { kRunTrain, "train" },
-  { kRunValidate, "validate" },
-  { kRunTest, "test" },
-});
-
-static std::map<int, int> percentage_for_run({
-  { kRunTest, 10 },
-  { kRunValidate, 20 },
-  { kRunTrain, 70 },
-});
-
 namespace caffe2 {
-
-void LoadLabels(const std::string &path_prefix, std::vector<std::string> &class_labels, std::vector<std::pair<std::string, int>> &image_files) {
-  std::cout << "load class labels.." << std::endl;
-  auto classes_text_path = path_prefix + "classes.txt";
-  ;
-  std::ifstream infile(classes_text_path);
-  std::string line;
-  while (std::getline(infile, line)) {
-    if (line.size()) {
-      class_labels.push_back(line);
-      // std::cout << '.' << line << '.' << std::endl;
-    }
-  }
-
-  std::cout << "load image folder.." << std::endl;
-  auto directory = opendir(FLAGS_folder.c_str());
-  CHECK(directory) << "~ no image folder " << FLAGS_folder;
-  if (directory) {
-    struct stat s;
-    struct dirent *entry;
-    while ((entry = readdir(directory))) {
-      auto class_name = entry->d_name;
-      auto class_path = FLAGS_folder + '/' + class_name;
-      if (class_name[0] != '.' && class_name[0] != '_' && !stat(class_path.c_str(), &s) && (s.st_mode & S_IFDIR)) {
-        auto subdir = opendir(class_path.c_str());
-        if (subdir) {
-          auto class_index = find(class_labels.begin(), class_labels.end(), class_name) - class_labels.begin();
-          if (class_index == class_labels.size()) {
-            class_labels.push_back(class_name);
-          }
-          while ((entry = readdir(subdir))) {
-            auto image_file = entry->d_name;
-            auto image_path = class_path + '/' + image_file;
-            if (image_file[0] != '.' && !stat(image_path.c_str(), &s) && (s.st_mode & S_IFREG)) {
-              // std::cout << class_name << ' ' <<  image_path << std::endl;
-              image_files.push_back({ image_path, class_index });
-            }
-          }
-          closedir(subdir);
-        }
-      }
-    }
-    closedir(directory);
-  }
-  CHECK(image_files.size()) << "~ no images found in " << FLAGS_folder;
-  std::random_shuffle(image_files.begin(), image_files.end());
-  std::cout << class_labels.size() << " labels found" << std::endl;
-  std::cout << image_files.size() << " images found" << std::endl;
-
-  std::cout << "write class labels.." << std::endl;
-  std::ofstream class_file(classes_text_path);
-  if (class_file.is_open()) {
-    for (auto &label: class_labels) {
-      class_file << label << std::endl;
-    }
-    class_file.close();
-  }
-  auto classes_header_path = path_prefix + "classes.h";
-  std::ofstream labels_file(classes_header_path.c_str());
-  if (labels_file.is_open()) {
-    labels_file << "const char * retrain_classes[] {";
-    bool first = true;
-    for (auto &label: class_labels) {
-      if (first) {
-        first = false;
-      } else {
-        labels_file << ',';
-      }
-      labels_file << std::endl << '"' << label << '"';
-    }
-    labels_file << std::endl << "};" << std::endl;
-    labels_file.close();
-  }
-}
-
-void WriteBatch(Workspace &workspace, NetBase *predict_net, std::string &input_name, std::string &output_name, std::vector<std::pair<std::string, int>> &batch_files, std::unique_ptr<db::DB> *database) {
-  std::unique_ptr<db::Transaction> transaction[kRunNum];
-  for (int i = 0; i < kRunNum; i++) {
-    transaction[i] = database[i]->NewTransaction();
-  }
-
-  std::vector<std::string> filenames;
-  for (auto &pair: batch_files) {
-    filenames.push_back(pair.first);
-  }
-  std::vector<int> indices;
-  auto input = readImageTensor(filenames, FLAGS_size_to_fit, indices);
-  TensorCPU output;
-  if (predict_net) {
-    set_tensor_blob(*workspace.GetBlob(input_name), input);
-    predict_net->Run();
-    auto tensor = get_tensor_blob(*workspace.GetBlob(output_name));
-    output.ResizeLike(tensor);
-    output.ShareData(tensor);
-  } else {
-    output.ResizeLike(input);
-    output.ShareData(input);
-  }
-
-  TensorProtos protos;
-  TensorProto* data = protos.add_protos();
-  TensorProto* label = protos.add_protos();
-  data->set_data_type(TensorProto::FLOAT);
-  label->set_data_type(TensorProto::INT32);
-  label->add_int32_data(0);
-  TensorSerializer<CPUContext> serializer;
-  std::string value;
-  std::vector<TIndex> dims(output.dims().begin() + 1, output.dims().end());
-  auto size = output.size() / output.dim(0);
-  auto output_data = output.data<float>();
-  for (auto i: indices) {
-    auto single = TensorCPU(dims, std::vector<float>(output_data, output_data + size), NULL);
-    output_data += size;
-    data->Clear();
-    serializer.Serialize(single, "", data, 0, kDefaultChunkSize);
-    label->set_int32_data(0, batch_files[i].second);
-    protos.SerializeToString(&value);
-    int percentage = 0;
-    for (auto pair: percentage_for_run) {
-      percentage += pair.second;
-      if (rand() % 100 < percentage) {
-        transaction[pair.first]->Put(batch_files[i].first, value);
-        break;
-      }
-    }
-  }
-
-  for (int i = 0; i < kRunNum; i++) {
-    transaction[i]->Commit();
-  }
-}
-
-void PreProcess(const std::vector<std::pair<std::string, int>> &image_files, const std::string *db_paths, NetDef &init_model, NetDef &predict_model, int batch_size) {
-  std::cout << "store partial prediction.." << std::endl;
-  std::unique_ptr<db::DB> database[kRunNum];
-  std::unique_ptr<db::Cursor> cursor[kRunNum];
-  for (int i = 0; i < kRunNum; i++) {
-    database[i] = db::CreateDB(FLAGS_db_type, db_paths[i], db::WRITE);
-    cursor[i] = database[i]->NewCursor();
-  }
-  auto image_count = 0;
-  Workspace workspace;
-  auto init_net = CreateNet(init_model, &workspace);
-  init_net->Run();
-  auto predict_net = predict_model.external_input_size() ? CreateNet(predict_model, &workspace) : NULL;
-  auto input_name = predict_model.external_input_size() ? predict_model.external_input(0) : "";
-  auto output_name = predict_model.external_output_size() ? predict_model.external_output(0) : "";
-  std::vector<std::pair<std::string, int>> batch_files;
-  for (auto &pair: image_files) {
-    auto &filename = pair.first;
-    auto class_index = pair.second;
-    image_count++;
-    auto in_db = false;
-    for (int i = 0; i < kRunNum && !in_db; i++) {
-      cursor[i]->Seek(filename);
-      in_db |= (cursor[i]->Valid() && cursor[i]->key() == filename);
-    }
-    if (!in_db) {
-      batch_files.push_back({ filename, class_index });
-    }
-    if (batch_files.size() == batch_size) {
-      std::cerr << '\r' << std::string(40, ' ') << '\r' << "pre-processing.. " << image_count << '/' << image_files.size() << " " << std::setprecision(3) << ((float)100 * image_count / image_files.size()) << "%" << std::flush;
-      WriteBatch(workspace, predict_net ? predict_net.get() : NULL, input_name, output_name, batch_files, database);
-      batch_files.clear();
-    }
-  }
-  if (batch_files.size() > 0) {
-    WriteBatch(workspace, predict_net ? predict_net.get() : NULL, input_name, output_name, batch_files, database);
-  }
-  for (int i = 0; i < kRunNum; i++) {
-    CHECK(database[i]->NewCursor()->Valid()) << "~ database " << name_for_run[i] << " is empty";
-  }
-  std::cerr << '\r' << std::string(80, ' ') << '\r' << image_files.size() << " images processed" << std::endl;
-}
 
 void run() {
   std::cout << std::endl;
@@ -240,13 +46,13 @@ void run() {
     return;
   }
 
-  if (!FLAGS_blob.size()) {
-    std::cerr << "specify a layer blob using --blob <name>" << std::endl;
+  if (!FLAGS_layer.size()) {
+    std::cerr << "specify a layer layer using --layer <name>" << std::endl;
     return;
   }
 
   std::cout << "model: " << FLAGS_model << std::endl;
-  std::cout << "blob_name: " << FLAGS_blob << std::endl;
+  std::cout << "layer: " << FLAGS_layer << std::endl;
   std::cout << "image_dir: " << FLAGS_folder << std::endl;
   std::cout << "db_type: " << FLAGS_db_type << std::endl;
   std::cout << "size_to_fit: " << FLAGS_size_to_fit << std::endl;
@@ -259,14 +65,14 @@ void run() {
 
   if (!FLAGS_force_cpu) setupCUDA();
 
-  std::string blob_safe = FLAGS_blob;
+  std::string layer_safe = FLAGS_layer;
   std::string::size_type index = 0;
-  while ((index = blob_safe.find("/", index)) != std::string::npos) {
-    blob_safe.replace(index, 3, "_");
+  while ((index = layer_safe.find("/", index)) != std::string::npos) {
+    layer_safe.replace(index, 3, "_");
     index += 1;
   }
 
-  auto path_prefix = FLAGS_folder + '/' + '_' + FLAGS_model + '_' + blob_safe + '_';
+  auto path_prefix = FLAGS_folder + '/' + '_' + FLAGS_model + '_' + layer_safe + '_';
   std::string db_paths[kRunNum];
   for (int i = 0; i < kRunNum; i++) {
     db_paths[i] = path_prefix + name_for_run[i] + ".db";
@@ -277,14 +83,12 @@ void run() {
   auto load_time = -clock();
   std::vector<std::string> class_labels;
   std::vector<std::pair<std::string, int>> image_files;
-  LoadLabels(path_prefix, class_labels, image_files);
+  LoadLabels(FLAGS_folder, path_prefix, class_labels, image_files);
 
   std::cout << "load model.." << std::endl;
   CHECK(ensureModel(FLAGS_model)) << "~ model " << FLAGS_model << " not found";
   NetDef full_init_model; // the original imagenet initialization model
   NetDef full_predict_model; // the original imagenet prediction model
-  NetDef pre_predict_model; // predict model for pre-processing
-  NetDef deploy_init_model; // the final initialization model
   NetDef init_model[kRunNum];
   NetDef predict_model[kRunNum];
   for (int i = 0; i < kRunNum; i++) {
@@ -295,126 +99,51 @@ void run() {
   std::string predict_filename = "res/" + FLAGS_model + "_predict_net.pb";
   CHECK(ReadProtoFromFile(init_filename.c_str(), &full_init_model)) << "~ empty init model " << init_filename;
   CHECK(ReadProtoFromFile(predict_filename.c_str(), &full_predict_model)) << "~ empty predict model " << predict_filename;
-  deploy_init_model.set_name("retrain_" + full_init_model.name());
-  pre_predict_model.set_name("pre_predict_model");
+
+  CheckLayerAvailable(full_predict_model, FLAGS_layer);
+
+  // std::cout << "full_init_model -------------" << std::endl;
+  // print(full_init_model);
+  // std::cout << "full_predict_model -------------" << std::endl;
+  // print(full_predict_model);
+
+  NetDef first_init_model, first_predict_model, second_init_model, second_predict_model;
+  SplitModel(full_init_model, full_predict_model, FLAGS_layer, first_init_model, first_predict_model, second_init_model, second_predict_model, FLAGS_force_cpu);
+
   if (!FLAGS_force_cpu) {
-    set_device_cuda_model(full_init_model);
-    set_device_cuda_model(full_predict_model);
-    set_device_cuda_model(pre_predict_model);
+    set_device_cuda_model(first_init_model);
+    set_device_cuda_model(first_predict_model);
+  }
+
+  // std::cout << "first_init_model -------------" << std::endl;
+  // print(first_init_model);
+  // std::cout << "first_predict_model -------------" << std::endl;
+  // print(first_predict_model);
+  // std::cout << "second_init_model -------------" << std::endl;
+  // print(second_init_model);
+  // std::cout << "second_predict_model -------------" << std::endl;
+  // print(second_predict_model);
+
+  for (int i = 0; i < kRunNum; i++) {
+    add_database_ops(init_model[i], predict_model[i], name_for_run[i], FLAGS_layer, db_paths[i], FLAGS_db_type, FLAGS_batch_size);
+  }
+  TrainModel(second_init_model, second_predict_model, FLAGS_layer, class_labels.size(), init_model[kRunTrain], predict_model[kRunTrain], FLAGS_learning_rate, FLAGS_optimizer);
+  TestModel(second_predict_model, predict_model[kRunValidate]);
+  TestModel(second_predict_model, predict_model[kRunTest]);
+
+  if (!FLAGS_force_cpu) {
     for (int i = 0; i < kRunNum; i++) {
       set_device_cuda_model(init_model[i]);
       set_device_cuda_model(predict_model[i]);
     }
   }
 
-  for (int i = 0; i < kRunNum; i++) {
-    add_database_ops(init_model[i], predict_model[i], name_for_run[i], FLAGS_blob, db_paths[i], FLAGS_db_type, FLAGS_batch_size);
-  }
-
-  std::cout << "split model.." << std::endl;
-  std::set<std::string> static_inputs;
-  std::string last_w, last_b;
-  std::vector<std::pair<std::string, std::string>> available_blobs;
-  auto blob_found = false;
-  std::set<std::string> strip_op_types({ "Dropout" });
-  for (const auto &op: full_predict_model.op()) {
-    if (op.input(0) != op.output(0)) {
-      available_blobs.push_back({ op.input(0), op.type() });
-      blob_found |= (op.input(0) == FLAGS_blob);
-    }
-    if (blob_found) {
-      auto train_only = (strip_op_types.find(op.type()) != strip_op_types.end());
-      for (int i = 0; i < (train_only ? 1 : kRunNum); i++) {
-        auto new_op = predict_model[i].add_op();
-        new_op->CopyFrom(op);
-        if (!FLAGS_force_cpu) {
-          set_engine_cudnn_op(*new_op);
-        }
-      }
-      if (op.type() == "FC") {
-        last_w = op.input(1);
-        last_b = op.input(2);
-      }
-    } else {
-      auto new_op = pre_predict_model.add_op();
-      new_op->CopyFrom(op);
-      if (!FLAGS_force_cpu) {
-        set_engine_cudnn_op(*new_op);
-      }
-      for (const auto &input: op.input()) {
-        static_inputs.insert(input);
-      }
-    }
-  }
-  if (!blob_found) {
-    std::cout << "available blobs:" << std::endl;
-    for (auto &blob: available_blobs) {
-      std::cout << "  " << blob.first << " (" << blob.second << ")" << std::endl;
-    }
-    LOG(FATAL) << "~ no blob with name " << FLAGS_blob << " in model.";
-  }
-  for (const auto &op: full_init_model.op()) {
-    auto &output = op.output(0);
-    if (static_inputs.find(output) == static_inputs.end()) {
-      auto init_op = init_model[kRunTrain].add_op();
-      bool uniform = (output.find("_b") != std::string::npos);
-      init_op->set_type(uniform ? "ConstantFill" : "XavierFill");
-      for (const auto &arg: op.arg()) {
-        if (arg.name() == "shape") {
-          auto init_arg = init_op->add_arg();
-          init_arg->set_name("shape");
-          if (output == last_w) {
-            init_arg->add_ints(class_labels.size());
-            init_arg->add_ints(arg.ints(1));
-          } else if (output == last_b) {
-            init_arg->add_ints(class_labels.size());
-          } else {
-            init_arg->CopyFrom(arg);
-          }
-        }
-      }
-      init_op->add_output(output);
-    }
-  }
-  auto op = init_model[kRunTrain].add_op();
-  op->set_type("ConstantFill");
-  auto arg = op->add_arg();
-  arg->set_name("shape");
-  arg->add_ints(1);
-  op->add_output(FLAGS_blob);
-  for (const auto &input: full_predict_model.external_input()) {
-    if (static_inputs.find(input) != static_inputs.end()) {
-      pre_predict_model.add_external_input(input);
-    } else if (input != FLAGS_blob) {
-      predict_model[kRunTrain].add_external_input(input);
-      predict_model[kRunValidate].add_external_input(input);
-      predict_model[kRunTest].add_external_input(input);
-    }
-  }
-  if (pre_predict_model.op().size()) {
-    pre_predict_model.add_external_output(FLAGS_blob);
-  }
-  for (const auto &output: full_predict_model.external_output()) {
-    for (int i = 0; i < kRunNum; i++) {
-      predict_model[i].add_external_output(output);
-    }
-  }
-  add_train_ops(init_model[kRunTrain], predict_model[kRunTrain], FLAGS_learning_rate, FLAGS_optimizer);
-  add_test_ops(predict_model[kRunValidate]);
-  add_test_ops(predict_model[kRunTest]);
-
-  // std::cout << "full_init_model -------------" << std::endl;
-  // print(full_init_model);
-  // std::cout << "full_predict_model -------------" << std::endl;
-  // print(full_predict_model);
-  // std::cout << "pre_predict_model -------------" << std::endl;
-  // print(pre_predict_model);
   // std::cout << "init_model[kRunTrain] -------------" << std::endl;
   // print(init_model[kRunTrain]);
   // std::cout << "predict_model[kRunTrain] -------------" << std::endl;
   // print(predict_model[kRunTrain]);
 
-  PreProcess(image_files, db_paths, full_init_model, pre_predict_model, FLAGS_batch_size);
+  PreProcess(image_files, db_paths, first_init_model, first_predict_model, FLAGS_db_type, FLAGS_batch_size, FLAGS_size_to_fit);
   load_time += clock();
 
 
@@ -467,6 +196,18 @@ void run() {
     }
   }
 
+  std::set<std::string> static_inputs;
+  for (const auto &op: full_predict_model.op()) {
+    if (op.input(0) != op.output(0) && op.input(0) == FLAGS_layer) {
+      break;
+    }
+    for (const auto &input: op.input()) {
+      static_inputs.insert(input);
+    }
+  }
+
+  NetDef deploy_init_model; // the final initialization model
+  deploy_init_model.set_name("retrain_" + full_init_model.name());
   for (const auto &op: full_init_model.op()) {
     auto &output = op.output(0);
     if (static_inputs.find(output) == static_inputs.end()) {
