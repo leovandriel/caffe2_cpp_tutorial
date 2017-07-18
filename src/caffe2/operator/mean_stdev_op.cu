493a5bc81cbc0d3d78aa79db098ba2d2f96d4c5a
Leo <leonardvandriel@gmail.com>
2017-07-18 16:36:05 -0700

    add dream on cuda

--- src/caffe2/operator/mean_stdev_op.cu
+++ src/caffe2/operator/mean_stdev_op.cu
@@ -5,34 +5,19 @@ namespace caffe2 {
 
 namespace {
 
-__global__ void ZeroKernel(const int N, float* X) {
+__global__ void MeanStdevKernel(const int N, const int C, const float D, const float* X, float* M, float* S) {
   CUDA_1D_KERNEL_LOOP(i, N) {
-   X[i] = 0;
-  }
-}
-
-__global__ void SumKernel(const int N, const int C, const float* X, float* M) {
-  CUDA_1D_KERNEL_LOOP(i, N) {
-    M[i / C] += X[i];
-  }
-}
-
-__global__ void ScaleKernel(const int N, const float C, float* M) {
-  CUDA_1D_KERNEL_LOOP(i, N) {
-    M[i] /= C;
-  }
-}
-
-__global__ void SumSqKernel(const int N, const int C, const float* X, const float* M, float* S) {
-  CUDA_1D_KERNEL_LOOP(i, N) {
-    float d = X[i] - M[i / C];
-    S[i / C] += d * d;
-  }
-}
-
-__global__ void SqrtKernel(const int N, const float C, float* S) {
-  CUDA_1D_KERNEL_LOOP(i, N) {
-    S[i] = sqrtf(S[i] / C);
+    float sum = 0;
+    for (int j = i * C, e = j + C; j != e; j++) {
+      sum += X[j];
+    }
+    M[i] = sum / D;
+    float sumsq = 0;
+    for (int j = i * C, e = j + C; j != e; j++) {
+      float v = X[j] - M[i];
+      sumsq += v * v;
+    }
+    S[i] = sqrtf(sumsq / D);
   }
 }
 
@@ -47,21 +32,8 @@ bool MeanStdevOp<float, CUDAContext>::RunOnDevice() {
   S->Resize(X.dim(0));
   if (X.size() > 0) {
     auto size = X.size() / X.dim(0);
-
-    ZeroKernel<<<CAFFE_GET_BLOCKS(M->size()), CAFFE_CUDA_NUM_THREADS, 0, context_.cuda_stream()>>>(
-      M->size(), M->mutable_data<float>());
-    ZeroKernel<<<CAFFE_GET_BLOCKS(S->size()), CAFFE_CUDA_NUM_THREADS, 0, context_.cuda_stream()>>>(
-      S->size(), S->mutable_data<float>());
-
-    SumKernel<<<CAFFE_GET_BLOCKS(X.size()), CAFFE_CUDA_NUM_THREADS, 0, context_.cuda_stream()>>>(
-      X.size(), size, X.data<float>(), M->mutable_data<float>());
-    ScaleKernel<<<CAFFE_GET_BLOCKS(M->size()), CAFFE_CUDA_NUM_THREADS, 0, context_.cuda_stream()>>>(
-      M->size(), (float)size, M->mutable_data<float>());
-
-    SumSqKernel<<<CAFFE_GET_BLOCKS(X.size()), CAFFE_CUDA_NUM_THREADS, 0, context_.cuda_stream()>>>(
-      X.size(), size, X.data<float>(), M->data<float>(), S->mutable_data<float>());
-    SqrtKernel<<<CAFFE_GET_BLOCKS(S->size()), CAFFE_CUDA_NUM_THREADS, 0, context_.cuda_stream()>>>(
-      S->size(), (float)size, S->mutable_data<float>());
+    MeanStdevKernel<<<CAFFE_GET_BLOCKS(M->size()), CAFFE_CUDA_NUM_THREADS, 0, context_.cuda_stream()>>>(
+      M->size(), size, (float)size, X.data<float>(), M->mutable_data<float>(), S->mutable_data<float>());
   }
 
   return true;
