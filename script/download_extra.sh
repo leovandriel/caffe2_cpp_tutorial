97cfc8c493cbfc60742b63b9130555f136e0c443
Leo Vandriel <git@leovandriel.com>
2017-07-28 11:16:16 +0100

    tweak dickens

--- script/download_extra.sh
+++ script/download_extra.sh
@@ -36,17 +36,17 @@ test -f res/resnet152_init_net.pb || echo "downloading ResNet-152 model (2)"
 test -f res/resnet152_predict_net.pb || curl --progress-bar --location --output res/resnet152_predict_net.pb https://github.com/leonardvandriel/caffe2_models/raw/master/model/resnet152_predict_net.pb
 test -f res/resnet152_init_net.pb || curl --progress-bar --location --output res/resnet152_init_net.pb https://github.com/leonardvandriel/caffe2_models/raw/master/model/resnet152_init_net.pb
 
-test -f res/dickens.txt || echo "downloading RNN and LSTM test data (10)"
-test -f res/dickens.txt || echo "" > res/dickens.tmp
-test -f res/dickens.txt || curl --progress-bar https://www.gutenberg.org/files/98/98-0.txt >> res/dickens.tmp
-test -f res/dickens.txt || curl --progress-bar https://www.gutenberg.org/files/1400/1400-0.txt >> res/dickens.tmp
-test -f res/dickens.txt || curl --progress-bar https://www.gutenberg.org/files/766/766-0.txt >> res/dickens.tmp
-test -f res/dickens.txt || curl --progress-bar https://www.gutenberg.org/files/786/786-0.txt >> res/dickens.tmp
-test -f res/dickens.txt || curl --progress-bar https://www.gutenberg.org/files/580/580-0.txt >> res/dickens.tmp
-test -f res/dickens.txt || curl --progress-bar https://www.gutenberg.org/files/883/883-0.txt >> res/dickens.tmp
-test -f res/dickens.txt || curl --progress-bar https://www.gutenberg.org/files/967/967-0.txt >> res/dickens.tmp
-test -f res/dickens.txt || curl --progress-bar https://www.gutenberg.org/files/963/963-0.txt >> res/dickens.tmp
-test -f res/dickens.txt || curl --progress-bar https://www.gutenberg.org/files/700/700-0.txt >> res/dickens.tmp
-test -f res/dickens.txt || curl --progress-bar https://www.gutenberg.org/files/821/821-0.txt >> res/dickens.tmp
-test -f res/dickens.txt || cat res/dickens.tmp | tr -cd 'A-Za-z0-9 \n!?,.:;()-' | tr -d '\r' | tr '\n' '~' | sed -e 's/~~/^/g' | tr '^' '\n' | tr '~' ' ' > res/dickens.txt
-test -f res/dickens.tmp && rm res/dickens.tmp
+test -f res/dickens_flat.txt || echo "downloading RNN and LSTM test data (10)"
+test -f res/dickens_flat.txt || echo "" > res/dickens.txt
+test -f res/dickens_flat.txt || curl --progress-bar https://www.gutenberg.org/files/98/98-0.txt >> res/dickens.txt
+test -f res/dickens_flat.txt || curl --progress-bar https://www.gutenberg.org/files/1400/1400-0.txt >> res/dickens.txt
+test -f res/dickens_flat.txt || curl --progress-bar https://www.gutenberg.org/files/766/766-0.txt >> res/dickens.txt
+test -f res/dickens_flat.txt || curl --progress-bar https://www.gutenberg.org/files/786/786-0.txt >> res/dickens.txt
+test -f res/dickens_flat.txt || curl --progress-bar https://www.gutenberg.org/files/580/580-0.txt >> res/dickens.txt
+test -f res/dickens_flat.txt || curl --progress-bar https://www.gutenberg.org/files/883/883-0.txt >> res/dickens.txt
+test -f res/dickens_flat.txt || curl --progress-bar https://www.gutenberg.org/files/967/967-0.txt >> res/dickens.txt
+test -f res/dickens_flat.txt || curl --progress-bar https://www.gutenberg.org/files/963/963-0.txt >> res/dickens.txt
+test -f res/dickens_flat.txt || curl --progress-bar https://www.gutenberg.org/files/700/700-0.txt >> res/dickens.txt
+test -f res/dickens_flat.txt || curl --progress-bar https://www.gutenberg.org/files/821/821-0.txt >> res/dickens.txt
+test -f res/dickens_flat.txt || cat res/dickens.txt | tr -cd 'A-Za-z0-9 \n!?,.:;()-' > res/dickens_strip.txt
+test -f res/dickens_flat.txt || cat res/dickens.txt | tr -cd 'A-Za-z0-9 \n!?,.:;()-' | tr -d '\r' | tr '\n' '~' | sed -e 's/~~/^/g' | tr '^' '\n' | tr '~' ' ' > res/dickens_flat.txt
