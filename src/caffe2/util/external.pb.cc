6787fecd732ab54c852d91145c6173605387291b
Leo <leonardvandriel@gmail.com>
2018-01-15 18:36:25 -0800

    add model bundle

---
+++ src/caffe2/util/external.pb.cc
@@ -0,0 +1,10 @@
+#include "external.pb.h"
+
+namespace protobuf_external_2eproto {
+  void InitDefaultsNetDef() {}
+  void AddDescriptors() {}
+};
+
+namespace caffe2 {
+  void protobuf_AddDesc_external_2eproto() {}
+}
