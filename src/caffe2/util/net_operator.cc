bedaa6121615b95b92f9af7aeae166836162463b
breadbread1984 <breadbread1984@163.com>
2018-02-13 19:50:39 +0800

    修改：     CMakeLists.txt 	修改：     src/caffe2/util/net_operator.cc 	新文件：   test/core/core_test.cc 	新文件：   test/util/net_gradient_test.cc 	新文件：   test/util/net_test.cc

--- src/caffe2/util/net_operator.cc
+++ src/caffe2/util/net_operator.cc
@@ -378,6 +378,7 @@ OperatorDef* NetUtil::AddMaxPoolOp(const std::string& input,
   net_add_arg(*op, "pad", padding);
   net_add_arg(*op, "kernel", kernel);
   net_add_arg(*op, "order", order);
+  net_add_arg(*op, "legacy_pad", 3);
   return op;
 }
 
@@ -390,6 +391,7 @@ OperatorDef* NetUtil::AddAveragePoolOp(const std::string& input,
   net_add_arg(*op, "pad", padding);
   net_add_arg(*op, "kernel", kernel);
   net_add_arg(*op, "order", order);
+  net_add_arg(*op, "legacy_pad", 3);
   return op;
 }
 
---
+++ test/util/net_test.cc
@@ -0,0 +1,22 @@
+#include "caffe2/util/net.h"
+#include <caffe2/core/init.h>
+#include <gtest/gtest.h>
+
+namespace caffe2 {
+
+TEST(NetUtilTest, TestName) {
+  NetDef model;
+  NetUtil net(model, "test");
+  EXPECT_EQ(model.name(), "test");
+}
+
+}  // namespace caffe2
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  caffe2::GlobalInit(&argc, &argv);
+  auto result = RUN_ALL_TESTS();
+  google::protobuf::ShutdownProtobufLibrary();
+  return result;
+}
+ 
