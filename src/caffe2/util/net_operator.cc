c8738ba45db71d6f15757ba92634e207f2fa9858
Leo Vandriel <git@leovandriel.com>
2018-02-12 16:57:15 -0800

    undo remove legacy pad

--- src/caffe2/util/net_operator.cc
+++ src/caffe2/util/net_operator.cc
@@ -319,6 +319,7 @@ OperatorDef* NetUtil::AddMaxPoolOp(const std::string& input,
   net_add_arg(*op, "pad", padding);
   net_add_arg(*op, "kernel", kernel);
   net_add_arg(*op, "order", order);
+  net_add_arg(*op, "legacy_pad", 3);
   return op;
 }
 
@@ -331,6 +332,7 @@ OperatorDef* NetUtil::AddAveragePoolOp(const std::string& input,
   net_add_arg(*op, "pad", padding);
   net_add_arg(*op, "kernel", kernel);
   net_add_arg(*op, "order", order);
+  net_add_arg(*op, "legacy_pad", 3);
   return op;
 }
 
